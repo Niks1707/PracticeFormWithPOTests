64b26d570172d75dcb1605f3d1055953241e0493
Veronika Genkel <genkel1707@gmail.com>
2024-10-20 19:37:59 +0200

    fix

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -7,7 +7,7 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 10000;
-
+        Configuration.timeout = 20000;  // Увеличенный тайм-аут для медленных запусков
+        Configuration.headless = true;  // Запуск в headless-режиме для работы без дисплея
     }
 }
