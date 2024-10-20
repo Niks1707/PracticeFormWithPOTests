b178b39d934dc5ef497468f9c18e8791324c6038
Veronika Genkel <genkel1707@gmail.com>
2024-10-20 19:59:29 +0200

    fix

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -7,6 +7,9 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 100000;  // Увеличенный тайм-аут для медленных запусков
+        Configuration.browser = System.getProperty("browserName","chrome");
+        Configuration.browserSize = System.getProperty("browserSize");
+        Configuration.browserVersion = System.getProperty("browserVersion");
+        Configuration.timeout = 10000;  // Увеличенный тайм-аут для медленных запусков
     }
 }
