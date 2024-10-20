1d407811e6c66258a32b4981b098b9de28d70c96
Veronika Genkel <genkel1707@gmail.com>
2024-10-20 20:09:34 +0200

    fix

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -9,6 +9,7 @@ public class TestBase {
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = "https://demoqa.com";
         Configuration.timeout = 10000;
     }
 }
