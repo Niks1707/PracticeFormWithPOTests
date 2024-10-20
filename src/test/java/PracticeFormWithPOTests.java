8776396b4962bb750c9d0044acee0e1b9cb79057
Veronika Genkel <genkel1707@gmail.com>
2024-10-20 19:50:22 +0200

    fix

--- src/test/java/PracticeFormWithPOTests.java
+++ src/test/java/PracticeFormWithPOTests.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.BeforeAll;
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
+        Configuration.browser = "edge";
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 20000;  // Увеличенный тайм-аут для медленных запусков
-        Configuration.headless = true;  // Запуск в headless-режиме для работы без дисплея
+        //Configuration.headless = true;  // Запуск в headless-режиме для работы без дисплея
+
     }
 }
