1e4f262804d855694d6bb4ab6a56305e7ccca234
Veronika Genkel <genkel1707@gmail.com>
2024-10-20 20:25:25 +0200

    fix

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -5,11 +5,12 @@ import org.junit.jupiter.api.BeforeAll;
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
-        WebDriverManager.chromedriver().setup();  // Настройка драйвера для Chrome
-        Configuration.browser = "chrome";
+        //WebDriverManager.chromedriver().setup();  // Настройка драйвера для Chrome
+        //Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
-        Configuration.pageLoadStrategy = "eager";
+        //Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.timeout = 10000;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 }
