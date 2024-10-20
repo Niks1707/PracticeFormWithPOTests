e1098fbea92f7e3ba45cdb74970655637b20dbe9
Veronika Genkel <genkel1707@gmail.com>
2024-10-20 19:54:00 +0200

    fix

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -4,12 +4,9 @@ import org.junit.jupiter.api.BeforeAll;
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
-        Configuration.browser = "edge";
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 20000;  // Увеличенный тайм-аут для медленных запусков
-        //Configuration.headless = true;  // Запуск в headless-режиме для работы без дисплея
-
+        Configuration.timeout = 100000;  // Увеличенный тайм-аут для медленных запусков
     }
 }
